5bd8bb60ec4b60b65d5f4044e330a30234ebfc70
“moshub-js” <avm.letters@gmail.com>
2023-04-09 14:54:41 +0300

    Модуль 2 задание  1

--- 02-event-loop/01-events-order/index.js
+++ 02-event-loop/01-events-order/index.js
@@ -1,23 +1,40 @@
+
+const fs = require('fs');
+const fileName = 'solution.txt';
+
+/* fs.open(fileName, 'w', (err, fd) => {
+  //fd - это дескриптор файла
+}) */
+
+const logger = fs.createWriteStream('solution.txt', { encoding: "utf-8", flags: "w" });
+
 const intervalId = setInterval(() => {
-  console.log('James');
+  console.log('James'); //1
+  logger.write('James\n');
 }, 10);
 
 setTimeout(() => {
   const promise = new Promise((resolve) => {
-    console.log('Richard');
-    resolve('Robert');
+    console.log('Richard'); //2
+    logger.write('Richard\n');
+    //fs.appendFileSync(fileName, 'Richard');
+    resolve('Robert'); //3
   });
 
   promise
-      .then((value) => {
-        console.log(value);
+    .then((value) => {
+      console.log(value); //robert
+      logger.write(`${value}\n`);
 
-        setTimeout(() => {
-          console.log('Michael');
+      setTimeout(() => {
+        console.log('Michael');//5
+        logger.write('Michael\n');
 
-          clearInterval(intervalId);
-        }, 10);
-      });
+        clearInterval(intervalId);
+      }, 10);
+    });
 
-  console.log('John');
+  console.log('John');//4
+  logger.write('John\n');
+  
 }, 10);
