5342d16b24edf13efbbffc85264edbfba9321e9d
“moshub-js” <avm.letters@gmail.com>
2023-04-13 13:37:17 +0300

    Модуль 3 задание - 1

--- 03-streams/01-limit-size-stream/LimitSizeStream.js
+++ 03-streams/01-limit-size-stream/LimitSizeStream.js
@@ -1,14 +1,27 @@
 const stream = require('stream');
 const LimitExceededError = require('./LimitExceededError');
 
+
+
 class LimitSizeStream extends stream.Transform {
   constructor(options) {
+    //console.log(options);
     super(options);
+    this.counter = 0;
+    this.limit = options.limit;
   }
 
   _transform(chunk, encoding, callback) {
-
-  }
+    this.counter = this.counter + chunk.length;
+    //console.log(chunk.toString());
+    //console.log(this.limit);
+    //console.log(this.counter);
+    let error = null;
+    if (this.counter > this.limit) {
+      error = new LimitExceededError();
+    }
+    callback(error, chunk);
+  };
 }
 
 module.exports = LimitSizeStream;
